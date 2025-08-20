24d9e000e00319761ef521d395376465c395dc6c
ElMatheus <matheus.goncalves5@aluno.senai.br>
2025-08-20 11:45:32 +0000

    feat: field idade

--- treino/treino/doctype/compradores/compradores.py
+++ treino/treino/doctype/compradores/compradores.py
@@ -3,7 +3,14 @@
 
 # import frappe
 from frappe.model.document import Document
-
+from frappe.utils.data import getdate, today
 
 class Compradores(Document):
-	pass
+  @property
+  def idade(self):
+    if self.data_nascimento:
+      nascimento = getdate(self.data_nascimento)
+      hoje = getdate(today())
+      
+      idade = hoje.year - nascimento.year - ((hoje.month, hoje.day) < (nascimento.month, nascimento.day))
+      return idade
